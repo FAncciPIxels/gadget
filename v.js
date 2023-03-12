6d69f5dac5194c30cef773e761f40bf28649ed43
FAncciPIxels <121621676+FAncciPIxels@users.noreply.github.com>
2023-03-12 14:20:17 -0700

    Add files via upload

---
+++ v.js
@@ -0,0 +1,53 @@
+// function turned() {
+//   let arr = "rotateY";
+//   let ar = ["Color", "red"];
+//   let tR = document.getElementById("fFace");
+//   tR.style.transform = arr + "(-180deg)";
+//   tR.style.backgroundColor = ar[1];
+// }
+// function colored(va) {
+//   let ob = { col: "black", bg: "white" };
+//   va.style.color = ob.col;
+//   va.style.backgroundColor = ob.bg;
+// }
+// function ret(val) {
+//   let obT = { colT: "green", bgT: "#00000033" };
+//   val.style.color = obT.colT;
+//   val.style.backgroundColor = obT.bgT;
+// }
+// function sc(v) {
+//   let mp = new Map();
+//   mp.set("scal", "scale");
+//   v.style.transform = mp.get("scal") + "(1.3)";
+// }
+
+// function le(v) {
+//   let arrr = ["scale", [1]];
+//   v.style.transform = arrr[0] + "(1)";
+// }
+
+// var a;
+// function mO() {
+//     a = document.querySelector("#aTop  a:nth-child(1)");
+    // for(let i = 0;i < a.length;i++) {
+        // let i = o;
+            // aforEach(colo);
+            // function colo(x) {
+        //     a.style.color = "coral"
+        //     // i++
+        // }
+
+
+
+        // function im(){
+
+            // let d =document.getElementById("imp").value;
+        //    let d =  document.getElementById("co").innerHTML;
+        //    d.replace("h", "popo");
+            // 
+        // }
+        // let c = Array.from(a[i].innerHTML)
+        // c.style.color = "coral";  
+    
+    // }
+// }
